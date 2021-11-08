afcf1025a1db065ad8c3b015416073d46ed8a586
deydrums <dagarcia100@gmail.com>
2021-11-08 16:22:39 -0600

    Dashboard

--- metaverso-app/src/components/metaverso/IndexScreen.js
+++ metaverso-app/src/components/metaverso/IndexScreen.js
@@ -2,8 +2,31 @@ import React from 'react'
 
 export const IndexScreen = () => {
     return (
-        <div>
-            Index
-        </div>
+        <>
+            <div className="border-bottom mt-2 mb-5">
+                <h1 className="mt-2 mt-md-4 mb-3 ">Dashboard</h1>
+            </div>
+
+            <div className="index_screen">
+                <div className="index_data">
+                    <div className="index_data_i">
+                        <div className="index_data_n">
+                            <h1>4</h1>
+                        </div>
+                        <div className="index_data_d">
+                            <h2>Usuarios</h2>
+                        </div>
+                    </div>
+                    <div className="index_data_i">
+                        <div className="index_data_n">
+                            <h1>8</h1>
+                        </div>
+                        <div className="index_data_d">
+                            <h2>Eventos</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
